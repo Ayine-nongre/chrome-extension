a40b7d460c17d2d4e2c92056c8492ce72a843942
Ayine-nongre <eugeneatinbire@gmail.com>
2023-10-01 11:21:22 +0000

    Fixing cors error 1

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const fs = require('fs')
 
 const app = express()
 
-app.use(cors())
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+}
+
+app.use(cors(corsOpts))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin',req.header('Origin'));
-    res.removeHeader('x-powered-by');
-    res.setHeader('Access-Control-Allow-Methods',req.method);
-    res.setHeader('Access-Control-Allow-Headers','Content-Type');
-    next();
-  })
 
 const uploadPath = process.cwd() + "/uploads/"
 let id = 10000
